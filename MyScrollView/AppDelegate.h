bf2dd271acb4dc936d4afbf47b78b4774478cc2f
Chloe Horgan <super.chloe@gmail.com>
2016-01-25 17:57:29 -0500

    Initial Commit

---
+++ MyScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyScrollView
+//
+//  Created by Chloe on 2016-01-25.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyScrollView
+//
+//  Created by Chloe on 2016-01-25.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
